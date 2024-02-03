80ecfbb33ee4363efd4ec50b62831d733a988b6d
Meer Salal Gurmani <77038502+meersalal@users.noreply.github.com>
2024-02-03 14:27:41 +0000

    dataType

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,13 @@
+"use strict"  // treat all Js code as newer version
+
+// number = 2 to power 53
+// boolen = true/false
+// string = ""
+// undefine = value not assign
+// bigint = for large value
+// null = standalone value
+// symbol = unique
+
+console.log(typeof undefined);  // return undefine
+console.log(typeof null);       // return object
+console.log(typeof "");
