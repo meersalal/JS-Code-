1e1a81a983e0798311199c9ff877323410effeb0
Meer Salal Gurmani <77038502+meersalal@users.noreply.github.com>
2024-02-06 12:21:43 +0000

    dataTypes

---
+++ 01_Basics/dataTypes_summary.js
@@ -0,0 +1,20 @@
+// primitive dataTpye
+
+// primitive: String, Number, Null, Undefine, BigInt, Symbol,Boolean
+
+// Reference(non primitive dataType)
+// Array, objects, Function
+
+// JavaScript is Dynamic Type language
+const Value = "123"  // Cannot define dataTypes in 
+
+let variable1 = String("345")
+let variable2 = Number(1234)
+let variable3 = null
+let variable4;
+
+let Id = Symbol("999")
+let anotherId = Symbol("999")
+
+console.log(Id === anotherId ) // false beacuse two different unique variable
+
