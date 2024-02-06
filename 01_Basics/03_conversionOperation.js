f26c9d850caf08c12d9bb4e760daae6657b1b748
Meer Salal Gurmani <77038502+meersalal@users.noreply.github.com>
2024-02-06 11:57:24 +0000

    conversion done

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,49 @@
+let score = null
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let ValueInNumber = Number(score)
+// console.log(typeof ValueInNumber);
+// console.log(ValueInNumber);
+
+// "33" = 33 
+// "33abc" = NaN
+// true = 1, false = 0
+
+let LoggedIn = ""
+
+let booleanLoggedIn = Boolean(LoggedIn)
+// console.log(booleanLoggedIn);
+
+// 1 => true, 0 => false
+// "" => false
+// "Meer" => true
+
+let value = 322
+
+ let stringValue = String(value)
+
+//  console.log(stringValue);
+//  console.log(typeof stringValue);
+
+console.log("1" + 2 + 2) 
+/* 
+return 122 beacuse if first value is in string then all value treat string
+*/ 
+console.log(1 + 2 + "2"); 
+/*
+return 32 beacuse if first value is not string then first complete the operation then add 
+the value of string 
+*/
+
+let gameCounter = 100
+
+let gameCounter2 = gameCounter++
+
+let gameCounter3 = ++gameCounter
+
+console.log(gameCounter);
+console.log(gameCounter2);
+console.log(gameCounter3);
+
