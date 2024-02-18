97aab9b407ecd0f8229c516675fd40d56cb86155
Meer Salal Gurmani <77038502+meersalal@users.noreply.github.com>
2024-02-18 17:22:28 +0000

    Strings

---
+++ 01_Basics/05_Strings.js
@@ -0,0 +1,41 @@
+const name = "Meer"
+const repoCount = 50
+
+// console.log(name + repoCount + "value"); 
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount} `);
+
+const gameName = new String("MeerKhan")
+
+
+//console.log(typeof gameName);  
+
+//console.log(gameName[3]); 
+
+//console.log(gameName.__proto__);
+
+// console.log(gameName.charAt(3));
+// console.log(gameName.indexOf('r'));
+
+const newString = gameName.substring(2,6)
+
+//console.log(newString);
+
+const anotherString = gameName.slice(-7,4)
+
+console.log(anotherString);
+
+console.log(gameName.length);
+
+const newStingOne = "      Meer Salal      "
+
+console.log(newStingOne)
+console.log(newStingOne.trim())
+
+const string = "Meer_Salal_Khan"
+
+const array = string.split("_")
+
+console.log(gameName.concat(3));
+
+
