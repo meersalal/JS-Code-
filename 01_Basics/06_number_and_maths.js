6e19ef6eec99328459f221d2df8351953f420598
Meer Salal Gurmani <77038502+meersalal@users.noreply.github.com>
2024-02-18 17:22:49 +0000

    Number and maths

---
+++ 01_Basics/06_number_and_maths.js
@@ -0,0 +1,37 @@
+const number = 200
+
+// console.log(number);
+
+const anotherNumer = new Number(100)
+// console.log(anotherNumer);
+
+// console.log(anotherNumer.toString());
+// console.log(anotherNumer.toFixed(2))
+
+const number2 = 112.924
+
+// console.log(number2.toPrecision(4))
+
+const balance = 1000000
+
+// console.log(balance.toLocaleString())
+
+/*************************   Maths *********************************/
+
+// console.log(Math)
+
+// console.log(Math.abs(-4))
+// console.log(Math.round(32.21))
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(2,9,5,1,0,4));
+// console.log(Math.max(2,9,5,1,0,4));
+
+console.log(Math.random());
+console.log(Math.floor((Math.random()*10) + 1));
+
+const min = 10 
+const max = 20
+
+console.log(Math.floor(Math.random()*(max - min + 1) + min))
+
