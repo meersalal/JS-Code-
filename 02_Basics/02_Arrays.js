381a22f23fec4e27652e4537e8ff6dde43e5011f
Meer Salal Gurmani <77038502+meersalal@users.noreply.github.com>
2024-02-18 17:24:46 +0000

    Arrays

---
+++ 02_Basics/02_Arrays.js
@@ -0,0 +1,33 @@
+const Students1 = ["Ali", "Zain", "Ahmand", "Yasir", "Daud"]
+const Students2 = ["Meer", "Hamza", "Abdullah"]
+const Students3 = ["Mehran", "Haider"]
+
+//Students1.push(Students2)
+//console.log(Students1[5][2]);
+
+// const AllStudents = Students1.concat(Students2)
+// console.log(AllStudents)
+
+// const All_New_Students = [...Students1, ...Students2, ...Students3]
+// console.log(All_New_Students)
+
+// const another_Array = [1,2,3,4,5,[6,7,8,9],10,11,12,13,14,[15,16,17,18,[19,20,21]],22]
+// const real_another_Array = another_Array.flat(Infinity)
+// console.log(real_another_Array)
+
+// check whathere it is array or not
+//console.log(Array.isArray("Meer"))   // this is not an array
+
+// convert an array 
+
+//console.log(Array.from("MeerSalal"))   //Convert into an Array
+const obj = {1:9,2:8,3:7}
+//console.log(Object.values(obj))
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+//console.log(Array.of(score1,score2,score3))
+
+console.log(years["2"] !== years["02"]);
