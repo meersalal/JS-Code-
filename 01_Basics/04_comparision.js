62cb577a9785baf2ee59096d8f8d7173781ea564
Meer Salal Gurmani <77038502+meersalal@users.noreply.github.com>
2024-02-18 17:22:13 +0000

    comparision

--- 01_Basics/04_comparision.js
+++ 01_Basics/04_comparision.js
@@ -1,2 +1,9 @@
-console.log("3" > 1);
+// In Javascript > < >= <= behaive different 
+console.log("3" > 1);  3 > 1 
 console.log("1" > 2);
+
+console.log(null > 0) // false beacuse null = 0 and 0 is not greater than 0
+
+console.log(null == 0); // false
+
+console.log(null >= 0); // true beacuse null = 0 and 0 is equall to 0
