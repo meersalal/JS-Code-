48e2857a29be5f9f2210086e08b913a428fa4221
Meer Salal Gurmani <77038502+meersalal@users.noreply.github.com>
2024-02-21 18:00:40 +0000

    Arrow_function

---
+++ 03_Basics/03_Arrow.js
@@ -0,0 +1,11 @@
+// const addOne = (number1, number2) => {
+//     return number1 + number2
+// }
+
+// const addOne = (number1, number2) =>  number1 + number2
+
+//const addOne = (number1, number2) =>  (number1 + number2)
+
+const User = () => ({username: "Meer"})             // object
+
+console.log(User())
