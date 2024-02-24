3bad7dfc92300380dc7018c72a8e4720c1cec0bd
Meer Salal Gurmani <77038502+meersalal@users.noreply.github.com>
2024-02-24 17:38:26 +0000

    iteration(for loop)

---
+++ 05_iterations/one.js
@@ -0,0 +1,30 @@
+// for (let i = 1; i <= 10; i++) {
+//     for (let j = 1; j <= 10; j++) {
+//         console.log(`${i} * ${j} = `+i*j);
+        
+//     }
+    
+// }
+
+//  break
+
+// for (let i = 0; i < 10; i++) {
+//     if (i == 5) {
+//         console.log(`Detected 5`);
+//         break
+//     }
+//     console.log(i);
+    
+// }
+
+
+//   Continue
+
+for (let i = 0; i < 10; i++) {
+    if (i == 5) {
+        console.log(`Detected 5`);
+        continue
+    }
+    console.log(i);
+    
+}
