6f0b28119528563cfbaebeaaaf6a0a355b85330a
Meer Salal Gurmani <77038502+meersalal@users.noreply.github.com>
2024-02-24 18:01:38 +0000

    while_and_dowhile_loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,15 @@
+// while loop
+let score = 1
+while (score <= 10) {
+    //console.log(score);
+    score = score + 1
+}
+
+// dowhile loop
+
+let score2 = 50
+
+do {
+    console.log(`Score is ${score2}`);
+    score2 = score2 + 5
+} while (score2 <= 50);
