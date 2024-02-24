9cb43fd33d65829890db635a5205aceb6963904b
Meer Salal Gurmani <77038502+meersalal@users.noreply.github.com>
2024-02-24 17:37:47 +0000

    switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,19 @@
+const month = 4
+
+switch (month) {
+    case 1:
+        console.log('janaury');
+        break;
+    case 2:
+        console.log('Feb');
+        break;
+    case 3:
+        console.log('march');
+        break;
+    case 4:
+        console.log('April');
+        break;        
+
+    default:
+        break;
+}
