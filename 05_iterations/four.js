d9852600559064eec686961d6f883b3efde7d56c
Meer Salal Gurmani <77038502+meersalal@users.noreply.github.com>
2024-02-28 12:48:16 +0000

    forin_loop

---
+++ 05_iterations/four.js
@@ -0,0 +1,16 @@
+const obj = {
+    js : 'javascript',
+    cpp: 'c++',
+    rb: 'ruby'
+}
+
+for (const key in obj) {
+   // console.log(`${key} shortcut is for ${obj[key]}`);
+}
+
+const programing = ['js','java','c++']
+
+for (const key in programing) {
+    console.log(programing[key]);
+}
+
